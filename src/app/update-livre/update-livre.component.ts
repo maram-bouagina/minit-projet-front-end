c812e6f9ffd99129437524f8948f2c7b3a68124d
Your Name <bouaginamaram6@gmail.com>
2024-11-30 12:33:43 +0100

    bkfrt2

--- src/app/update-livre/update-livre.component.ts
+++ src/app/update-livre/update-livre.component.ts
@@ -26,41 +26,18 @@ loading : boolean = false;
       constructor(private activatedRoute: ActivatedRoute,
         private router :Router,
       private livreService: LivreService,private formBuilder: FormBuilder) { }
+
+
     ngOnInit() {
-      const idLivre = this.activatedRoute.snapshot.params['id'];
-      console.log('ID du livre récupéré :', idLivre);
       this.livreService.listegenres().
-      subscribe(gens => {this.genres = gens._embedded.genres;
+      subscribe(gens => {this.genres =gens._embedded.genres;
       console.log(gens);
       });
-     
-
-      if (idLivre) {
-    
-        this.livreService.consulterlivre(idLivre).subscribe(livre => {
-          this.currentlivre = livre;
-          this.updatedgenId=this.currentlivre.genre.idgenre;
-          if (this.currentlivre) {
-            console.log('Livre récupéré :', this.currentlivre);
-    
-           
-            this.myForm = this.formBuilder.group({
-              idlivre: [this.currentlivre.idlivre, [Validators.required]],
-              titre: [this.currentlivre.titre, [Validators.required, Validators.minLength(6)]],
-              auteur: [this.currentlivre.auteur, [Validators.required, Validators.minLength(6)]],
-              nbpages: [this.currentlivre.nbpages, [Validators.required]],
-              datepublication: [this.currentlivre.datepublication, [Validators.required]],
-               idgenre: [this.currentlivre.genre.idgenre, [Validators.required]]
-            });
-          } else {
-            console.error('Le livre récupéré est vide');
-          }
-        }, error => {
-          console.error('Erreur lors de la récupération du livre:', error);
-        });
-      } else {
-        console.error('ID du livre est invalide');
-      }
+      this.livreService.consulterlivre(this.activatedRoute.snapshot.params['id']).
+      subscribe( prod =>{ this.currentlivre = prod; 
+      this.updatedgenId = 
+      this.currentlivre.genre.idgenre;
+      } ) ;
     }
     
     
